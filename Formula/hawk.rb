d27650d844928070b757940b91542a1d36b3d208
nakachan-ing <79064717+nakachan-ing@users.noreply.github.com>
2025-07-11 23:38:27 +0900

    Update function for data fields select

--- Formula/hawk.rb
+++ Formula/hawk.rb
@@ -2,7 +2,7 @@ class Hawk < Formula
   desc "Modern data analysis tool for structured data (JSON, YAML, CSV)"
   homepage "https://github.com/kyotalab/hawk"
   url "https://github.com/kyotalab/hawk/releases/download/v0.1.0/hawk"
-  sha256 "cf81287062e153317322bf0adf9c4b6dc5dac309bcc7a56722b6b4e16a9799b6"
+  sha256 "0b78ff35e31b552112a99c55a8a83184198635fe9d02361adde1453cf0bd4413"
   version "0.1.0"
 
   def install
