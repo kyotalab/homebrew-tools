69bb935d6d0a62e2d345b8b7214a5ec0ccdb3435
nakachan-ing <79064717+nakachan-ing@users.noreply.github.com>
2025-07-18 16:26:43 +0900

    Update version from v0.2.1 to v0.2.2

--- Formula/hawk.rb
+++ Formula/hawk.rb
@@ -1,9 +1,9 @@
 class Hawk < Formula
   desc "Modern data analysis tool for structured data (JSON, YAML, CSV)"
   homepage "https://github.com/kyotalab/hawk"
-  url "https://github.com/kyotalab/hawk/releases/download/v0.2.1/hawk"
-  sha256 "d1ddf873a8f99f4b800fb1e2c13d6551a11a8734610ce2585ac156418799c456"
-  version "0.2.1"
+  url "https://github.com/kyotalab/hawk/releases/download/v0.2.2/hawk"
+  sha256 "3d85f7a7e801a39e5702f6af31721d2fb2d32caaae50556ff733a9775989cf98"
+  version "0.2.2"
 
   def install
     bin.install "hawk"
