a9d2ea8fddd2cf8f70b8d27baf9305253d34fc37
kyotalab <jagbvg.eptpeox@gmail.com>
2026-03-16 01:08:06 +0900

    Update formula tabstruct

--- Formula/tabstruct.rb
+++ Formula/tabstruct.rb
@@ -1,20 +1,20 @@
 class Tabstruct < Formula
   desc "Inspect and convert CSV, JSON, and YAML with nested structure support"
   homepage "https://github.com/kyotalab/tabstruct"
-  version "0.1.1"
+  version "0.1.5"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/kyotalab/tabstruct/releases/download/v0.1.1/tabstruct-v0.1.1-aarch64-apple-darwin.tar.gz"
-      sha256 "6b02cb08af00f799fd908af017847249120c770b501646ba86b7c1620f6c8ef6"
+      url "https://github.com/kyotalab/tabstruct/releases/download/v0.1.5/tabstruct-v0.1.5-x86_64-apple-darwin.tar.gz"
+      sha256 "f980909e20eb015557aec7dbaef9fe1c8a5ff6c47db18fb0338bcd525e1b8ccb"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/kyotalab/tabstruct/releases/download/v0.1.1/tabstruct-v0.1.1-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "0bd98e49d63dea51f7dd151106a109753bc01825ffb24d6883c78d8c4b4002c2"
+      url "https://github.com/kyotalab/tabstruct/releases/download/v0.1.5/tabstruct-v0.1.5-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "3a4f8b56011de14e0b8ee40d7ec41aa5487213fb492775f406beaae41d2f1811"
     end
   end
 
